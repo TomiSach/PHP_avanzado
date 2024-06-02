b2bfd8229a757e6d2e4c57347e0a4a4f4b6f23a6
TomiSach <tomassachini@hotmail.com>
2024-06-01 23:02:02 -0300

    listillo

--- base_evaluaciones/marca_de_agua.php
+++ base_evaluaciones/marca_de_agua.php
@@ -21,7 +21,7 @@ $img2_width = imagesx($img2);
 $img2_height = imagesy($img2);
 $img_width = imagesx($img);
 $img_height = imagesy($img);
-imagecopy
+
 imagecopy($img2, $img, ($img2_width / 2) - ($img_width / 2), ($img2_height / 2) - ($img_height / 2), 0, 0, $img_width, $img_height);
 header('Content-type: image/jpeg');
 imagejpeg($img2);
